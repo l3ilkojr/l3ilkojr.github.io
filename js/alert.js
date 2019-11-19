7d9a28ddcfdff987da70b62d8ac67d14a42f4f15
l3ilko Jr <54482782+l3ilko-Jr@users.noreply.github.com>
2019-11-19 11:30:01 -0500

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -2,4 +2,4 @@ function update() {
 setTimeout(function() { alert("Testing Again (5 seconds!)"; }, 5000);
 }
            
-// Update Command waits for site to load then comes up.
+update(); // Update Command waits for site to load then comes up.
