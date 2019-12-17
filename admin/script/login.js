33a39fb9b7c10f63d54b171f9dd5b0ebe42efc12
l3ilko Jr <50190264+l3ilkojr@users.noreply.github.com>
2019-12-17 13:28:44 -0500

    Update login.js

--- admin/script/login.js
+++ admin/script/login.js
@@ -6,21 +6,19 @@ const msg= document.querySelector(".msg");
 // Listen for a login event on the form
 login.addEventListener('submit', onLogin);
 
+
+
 function onLogin(e) {
     e.preventDefault();
-
-    // Form validation
-    if (userName.value === '' || password.value === '') {
-        msg.innerHTML = 'Please enter username and password';
-        setTimeout(() => msg.remove() , 3000);
+    
+    if (uerName.value === '' || password.value === '') {
+        msg.innerHTML = 'Please If your not a admin don\'t try login.';
+        setTimeout(() => msg.remove(), 3000);
     } else {
-
-        // Validate the userName and password
-        if (userName.value === 'admin' && password.value === 'password') {
-            msg.innerHTML = 'Enter the correct username and password'
-            setTimeout(() => msg.remove(), 3000);
+        if (userName.value === 'admin' && password.value === 'adminlogin') {
+            window.location.href = "https://l3ilkojr.github.io/admin/adminpage.html
         } else {
-            alert('Login success');
+            alert('Nope');
         }
     }
 }
